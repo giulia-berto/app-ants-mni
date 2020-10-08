6e99e4bbb2189ad4a67fb25907510a2818deafc0
Giulia Berto <gberto@fbk.eu>
2020-10-08 16:07:45 +0200

    dockerized app + other updates

---
+++ ants_t1w_transformation.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+T1W_MOVE=$1
+T1W_STAT=$2
+
+T1W_M=space-dwi_T1w.nii.gz
+T1W_ANT_PRE=space_MNI_var-t1w_
+
+#------------------------------------------------------------------------------
+# ANTS Registration of structural images
+#------------------------------------------------------------------------------
+
+BRAIN_MASK=`jq -r '.mask' config.json`
+
+# Brain extraction 
+if [ $BRAIN_MASK == null ]; then
+	echo "Brain extraction using BET"
+	bet $T1W_MOVE $T1W_M
+else
+	echo "Brain extraction using mask provided"
+	fslmaths $T1W_MOVE -mas $BRAIN_MASK $T1W_M
+fi
+
+T1W_S=$T1W_STAT #already without skull
+
+# Warp Computation
+ANTS 3 -m CC[${T1W_S},${T1W_M},1,5] -t SyN[0.5] \
+    -r Gauss[2,0] -o $T1W_ANT_PRE -i 30x90x20 --use-Histogram-Matching
+
+#antsRegistrationSyNQuick.sh -d 3 \
+#     -f $T1W_STAT -m $T1W_MOVE -t s -o $T1W_ANT_PRE
+
+
+#------------------------------------------------------------------------------
+# Renaming files
+#------------------------------------------------------------------------------
+
+mv ${T1W_ANT_PRE}Warp.nii.gz ${T1W_ANT_PRE}warp.nii.gz
+mv ${T1W_ANT_PRE}InverseWarp.nii.gz ${T1W_ANT_PRE}invwarp.nii.gz
+mv ${T1W_ANT_PRE}Affine.txt ${T1W_ANT_PRE}affine.txt
+
+cp ${T1W_ANT_PRE}warp.nii.gz warps/warp.nii.gz
+cp ${T1W_ANT_PRE}invwarp.nii.gz warps/inverse-warp.nii.gz
+
+#------------------------------------------------------------------------------
+# Convert the warp for tck
+#------------------------------------------------------------------------------
+
+WARP_PRE=space_MNI_var-ant_warp
+WARP_INI=space_MNI_var-ant_warp-i[].nii.gz
+WARP_TMP=space_MNI_var-ant_warp-t[].nii.gz
+WARP_T1W=space_MNI_var-t1w4tck_warp.nii.gz
+
+T1W_AFF=${T1W_ANT_PRE}affine.txt
+T1W_IWARP=${T1W_ANT_PRE}invwarp.nii.gz
+T1W_IWARP_FIX=${T1W_ANT_PRE}InverseWarp.nii.gz
+
+warpinit ${T1W_S} ${WARP_INI} -force -quiet
+cp $T1W_IWARP $T1W_IWARP_FIX
+for i in 0 1 2; do
+    WarpImageMultiTransform 3 \
+        ${WARP_PRE}-i${i}.nii.gz ${WARP_PRE}-t${i}.nii.gz \
+        -R $T1W_S -i $T1W_AFF $T1W_IWARP_FIX
+done
+rm -f $T1W_IWARP_FIX
+warpcorrect $WARP_TMP $WARP_T1W -force -quiet
+rm ${WARP_PRE}-*
+rm $T1W_M
+rm $T1W_IWARP
---
+++ run_registration.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+t1=$1
+tck=$2
+
+atlas=MNI152_T1_1.25mm_brain.nii.gz
+
+if [ -f track_aligned/track.tck ]; then 
+    echo "Tractogram already registered in MNI space."
+else 
+    echo "Computing warp to MNI space..."
+    ./ants_t1w_transformation.sh $t1 $atlas
+
+    mv space_MNI_var-t1w_affine.txt warp_dir
+	mv space_MNI_var-t1w_warp.nii.gz warp_dir
+	mv space_MNI_var-t1w4tck_warp.nii.gz warp_dir
+
+	if [ -z "$(ls -A -- warp_dir)" ]; then    
+		echo "Transformation failed."
+		exit 1
+	else    
+		echo "Transformation done."
+	fi	
+
+	echo "Registering the tractogram to MNI space..."
+	tcktransform $tck warp_dir/space_MNI_var-t1w4tck_warp.nii.gz \
+	             track_aligned/track.tck -nthreads 0 -force -quiet
+
+	if [ -z "$(ls -A -- track_aligned)" ]; then    
+		echo "Tractogram registration failed."
+		exit 1
+	else    
+		echo "Tractogram registration done."
+	fi
+
+fi
+
+if [ -f t1_aligned/t1.nii.gz ]; then
+	echo "T1 already registered in MNI space."
+else
+	echo "Registering the T1 to MNI space..."
+	WarpImageMultiTransform 3  \
+	    	$t1 \
+	    	t1_aligned/t1.nii.gz \
+	    	warp_dir/space_MNI_var-t1w_warp.nii.gz \
+	    	warp_dir}/space_MNI_var-t1w_affine.nii.txt  --use-NN
+
+	if [ -z "$(ls -A -- t1_aligned)" ]; then    
+		echo "T1 registration failed."
+		exit 1
+	else    
+		echo "T1 registration done."
+	fi    	
+fi
